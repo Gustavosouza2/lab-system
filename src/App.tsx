421d7b84d153ba8c8183109f8fef0669a1709d50
Gustavosouza2 <gustavoleonsouza@gmail.com>
2022-10-13 18:18:57 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/global.css';
+
+
+export function App() {
+  return (
+    <>
+      <h1 className="font-bold text-2xl text-violet-500">Hello World!</h1>
+      <button className="bg-cyan font-medium px-4 py-3 rounded text-white">enviar</button>
+    </>
+  )
+}
+
+
---
+++ src/components/Text.stories copy.tsx
@@ -0,0 +1,55 @@
+import  { Meta, StoryObj} from '@storybook/react'
+import { Text, TextProps } from "./Text";
+
+export default {
+    title: 'Components/Text',
+    component: Text,
+    args: {
+        children: "I'm a Text",
+        size: 'md',
+    },
+    argTypes: {
+        size: {
+            options: ['sm', 'md', 'lg'],
+            control: {
+                type: 'inline-radio'
+            }
+        }
+    }
+    
+} as  Meta <TextProps>
+
+export const Default: StoryObj<TextProps> = {}
+
+
+export const Small: StoryObj<TextProps> = {
+    args: {
+        size: "sm",  
+    }
+}
+
+export const Large: StoryObj<TextProps> = {
+    args: {
+        size: "lg",  
+    }
+}
+export const CustomComponent: StoryObj<TextProps> = {
+    args: {
+       asChild: true,
+       children: (
+        <p>Text With P tag</p>
+       )  
+    },
+    argTypes: {
+        children: {
+            table: {
+                disable: true,
+            }
+        },
+        asChild: {
+            table: {
+                disable: true,
+            }
+        }
+    }
+}
